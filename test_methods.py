a30384648d0425d46f7e1472a4628b067073684f
MURILO WIERMANN TEIXEIRA <22.01578-7@maua.br>
2023-05-04 08:57:31 -0300

    exercicio de desenvolvimento agil calculadora

--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,44 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_soma():
+    a = 2
+    b = 5
+
+    output = methods.soma(a, b)
+    
+    assert output == 7
+
+def test_subtracao():
+    a = 4
+    b = 1
+
+    output = methods.subtracao(a, b)
+    
+    assert output ==3
+
+def test_divisao():
+    a = 8
+    b = 4
+
+    output = methods.divisao(a, b)
+    
+    assert output == 2
+
+def test_multiplicacao():
+    a = 2
+    b = 5
+
+    output = methods.multiplicacao(a, b)
+    
+    assert output == 10
+
+def test_potencia():
+    a = 2
+
+    output = methods.potencia(a)
+    
+    assert output == 4
+
+    
